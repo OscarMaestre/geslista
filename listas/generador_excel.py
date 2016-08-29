1249ed8c6114a29a9ba6d0803dcf5facadd42b68
oscar <oscar@example.com>
2016-08-29 10:00:11 +0200

    Generacion mas completa

--- listas/generador_excel.py
+++ listas/generador_excel.py
@@ -23,16 +23,63 @@ def generar_archivo_excel(fecha_procedimiento, cod_especialidad):
     nombramientos=Nombramiento.objects.filter(
         especialidad__codigo_especialidad__contains=cod_especialidad
     ).order_by ( "numero_orden", "nombre_completo")
+    
     print ("Generando XLS para "+nombre+".XLS")
     escritor=EscritorExcel(nombre+".xls")
     escritor.anadir_hoja(nombre)
-    escritor.escribir_en_hoja(0, 0, "Total llamados");
-    escritor.escribir_en_hoja(0, 1, len(nombramientos));
-    escritor.escribir_en_hoja(2, 0, "Num orden");
-    escritor.escribir_en_hoja(2, 1, "Nombre");
-    escritor.escribir_en_hoja(2, 2, "Centro");
-    escritor.escribir_en_hoja(2, 3, "Localidad");
-    num_fila=3
+    num_fila=0
+    
+    escritor.escribir_en_hoja(num_fila, 0, "Total llamados");
+    escritor.escribir_en_hoja(num_fila, 1, len(nombramientos));
+    num_fila+=1
+    
+    nombramientos_tp=nombramientos.filter(
+        especialidad__codigo_especialidad__contains=cod_especialidad,
+        especialidad__tipo_de_jornada=Especialidad.MEDIA_JORNADA
+    )
+    escritor.escribir_en_hoja(num_fila, 0, "T. parciales");
+    escritor.escribir_en_hoja(num_fila, 1, len(nombramientos_tp));
+    num_fila+=1
+
+    nombramientos_completa=nombramientos.filter(
+        especialidad__codigo_especialidad__contains=cod_especialidad,
+        especialidad__tipo_de_jornada=Especialidad.JORNADA_COMPLETA
+    )
+    escritor.escribir_en_hoja(num_fila, 0, "T. completo");
+    escritor.escribir_en_hoja(num_fila, 1, len(nombramientos_completa));
+    num_fila+=1
+    
+    nombramientos_espanol=nombramientos.filter(
+        especialidad__codigo_especialidad__contains=cod_especialidad,
+        especialidad__idioma=Especialidad.IDIOMA_ESPANOL
+    )
+    escritor.escribir_en_hoja(num_fila, 0, "No bilingües");
+    escritor.escribir_en_hoja(num_fila, 1, len(nombramientos_espanol));
+    num_fila+=1
+    
+    nombramientos_ingles=nombramientos.filter(
+        especialidad__codigo_especialidad__contains=cod_especialidad,
+        especialidad__idioma=Especialidad.IDIOMA_INGLES
+    )
+    escritor.escribir_en_hoja(num_fila, 0, "Bil ingles");
+    escritor.escribir_en_hoja(num_fila, 1, len(nombramientos_ingles) );
+    num_fila+=1
+    
+    nombramientos_frances=nombramientos.filter(
+        especialidad__codigo_especialidad__contains=cod_especialidad,
+        especialidad__idioma=Especialidad.IDIOMA_FRANCES
+    )
+    escritor.escribir_en_hoja(num_fila, 0, "Bil frances");
+    escritor.escribir_en_hoja(num_fila, 1, len(nombramientos_frances) );
+    num_fila+=1
+
+    
+    num_fila+=2
+    escritor.escribir_en_hoja(num_fila, 0, "Num orden");
+    escritor.escribir_en_hoja(num_fila, 1, "Nombre");
+    escritor.escribir_en_hoja(num_fila, 2, "Centro");
+    escritor.escribir_en_hoja(num_fila, 3, "Localidad");
+    num_fila+=1
     for n in nombramientos:
         escritor.escribir_en_hoja(num_fila, 0, n.numero_orden)
         escritor.escribir_en_hoja(num_fila, 1, n.nombre_completo)
@@ -42,6 +89,13 @@ def generar_archivo_excel(fecha_procedimiento, cod_especialidad):
             escritor.escribir_en_hoja(num_fila, 4, "")
         else:
             escritor.escribir_en_hoja(num_fila, 4, "T. parcial")
+        if n.especialidad.idioma==Especialidad.IDIOMA_ESPANOL:
+            escritor.escribir_en_hoja(num_fila, 5, "")
+        else:
+            if n.especialidad.idioma==Especialidad.IDIOMA_INGLES:
+                escritor.escribir_en_hoja(num_fila, 5, "Bil ingles")
+            else:
+                escritor.escribir_en_hoja(num_fila, 5, "Bil frances")
         num_fila+=1
      
         
